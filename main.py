f5447ac5ed329485563f3f30469f31fd61f3593d
Angelion879 <diangelis879@outlook.com>
2025-02-11 15:34:47 -0300

    (feat) main function

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-from imgWriter import imgWritter
+from imgWriter import img_writer
+from emailSender import email_sender
+from spreadsheeManager import build_code, append_client
 
-imgWritter("Suri Carla","1001")
+def main():
+    name_list = open('names.txt','r')
+
+    for line in name_list:
+        name = ' '.join(line.split(" ")[:2])
+        email = line.split(" ")[-1]
+
+        client_code = build_code()
+        print(name)
+        img_writer(name,str(client_code))
+        append_client(name,email,client_code)
+        email_sender(email)
+
+main()
