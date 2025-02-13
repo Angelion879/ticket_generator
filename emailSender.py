167c137851e258f4580c873df5ae8d2b218b24d3
Angelion879 <diangelis879@outlook.com>
2025-02-13 01:18:47 -0300

    (update) email tittle

--- emailSender.py
+++ emailSender.py
@@ -9,7 +9,7 @@ def email_sender(email):
     msg = MIMEMultipart()
     msg['from'] = 'Suricatos'
     msg['to'] = email
-    msg['subject'] = 'Ingresso do Suricato!'
+    msg['subject'] = 'Ingresso dos Suricatos!'
 
     email_body = MIMEText("Aqui está seu ingresso para a nossa festa! \nSalve a imagem e/ou guarde este email para apresentar na entrada do evento! \nNos vemos na nossa toca!")
     msg.attach(email_body)
@@ -25,5 +25,3 @@ def email_sender(email):
         smtp.send_message(msg)
 
         print('Mail Delivered!')
-
-email_sender('bibs879@gmail.com')
