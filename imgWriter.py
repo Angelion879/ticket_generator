b99a47ce39259d4f6b0ce332fdfe006efce75002
Angelion879 <diangelis879@outlook.com>
2025-02-13 15:11:27 -0300

    (update) image and text font / size

--- imgWriter.py
+++ imgWriter.py
@@ -1,12 +1,15 @@
 import os
 from PIL import Image, ImageDraw, ImageFont
 
+color = (0,0,0)
+y_axis = 1620
+
 def img_writer (name,code):
     with Image.open('PASSAPORTE_suricato.png') as img:
         draw = ImageDraw.Draw(img)
-        new_font = ImageFont.truetype("arial.ttf", 25)
-        draw.text((250,220),name,(61, 52, 235),font=new_font)
-        draw.text((300,250),code,(61, 52, 235),font=new_font)
+        new_font = ImageFont.truetype("nats.ttf", 100)
+        draw.text((70,y_axis),name,color,font=new_font)
+        draw.text((1000,y_axis),code,color,font=new_font)
 
         if os.path.exists('passaporte.jpg'):
             os.remove('passaporte.png')
